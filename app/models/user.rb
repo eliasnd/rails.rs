8ce491907ac51eb52783f9c478f6956f8394d1d5
eliasnd <elias@ndcoding.com>
2020-07-27 15:40:09 -0400

    Finished tutorial

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+
+	has_secure_password
+
+	validates :username, presence: true
+	validates_uniqueness_of :username
+	validates :password, presence: true, length: { minimum: 8 }
+
+end
