ad77704fab56cf3f313bf49fb0b04b0daa104941
eliasnd <elias@ndcoding.com>
2020-07-28 11:03:42 -0400

    Validation working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,18 @@ class UsersController < ApplicationController
 	skip_before_action :authorized, only: [:new, :create]
 
 	def new
+		@user = User.new
 	end
 
 	def create
-		@user = User.create(user_params)
+		@user = User.new(user_params)
 
-		session[:user_id] = @user.id
-
-		redirect_to '/welcome'
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to '/welcome'
+		else
+			render 'new'
+		end
 	end
 
 	private
